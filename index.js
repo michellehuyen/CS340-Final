299ef6ec373d0e9b25bc6f9d6476ef29722f0615
nguyemi4 <nguyemi4@oregonstate.edu>
2023-07-22 20:38:04 -0700

    changed port #

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8001;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8000;                 // Set a port number at the top so it's easy to change in the future
 const path = require('path');
 
 // Database
