44d59c65fae86e44cee37b126c3618f2bc19d884
Preston Sellers <sellersp@flip1.engr.oregonstate.edu>
2023-08-06 21:56:00 -0700

    switched port to 8006

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-PORT        = 8009;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8006;                 // Set a port number at the top so it's easy to change in the future
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
